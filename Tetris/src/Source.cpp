a6b76fbbc8fb97e13df448231669b9f5e5099e61
MikolajGajos <mikus22500@gmail.com>
2022-04-19 20:43:45 +0200

    .

--- Tetris/src/Source.cpp
+++ Tetris/src/Source.cpp
@@ -2,23 +2,8 @@
 
 #include "GameApp.h"
 
-int main()
-{	
-	GameApp app(0);
-
-	std::cout << "score: " << app.run() << std::endl;
-
-	/*sf::RenderWindow window(sf::VideoMode(800, 600), "xd");
-	sf::Font font;
-	font.loadFromFile("src/rsrc/slkscr.ttf");
-	sf::Text text;
-	text.setFont(font);
-	text.setCharacterSize(150);
-	text.setString("GAME\nOVER");
-	while (window.isOpen())
-	{
-		window.clear(sf::Color::Black);
-		window.draw(text);
-		window.display();
-	}*/
+void main()
+{
+    GameApp app(0);
+    std::cout << "score: " << app.run() << std::endl;
 }
