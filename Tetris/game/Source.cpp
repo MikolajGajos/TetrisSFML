787cfc25aca33d60b35b8eb1daa172f04cb92842
MikolajGajos <mikus22500@gmail.com>
2022-06-04 17:56:04 +0200

    sfml files macro

--- Tetris/game/Source.cpp
+++ Tetris/game/Source.cpp
@@ -1,7 +1,7 @@
 #include "../menu/MenuClass.h"
 #include <Windows.h>
 
-void main()
+int main()
 {
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
